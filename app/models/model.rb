306dcb8d9ffd3caaaf3ed2312da08d98c6afc41c
Jiaming Lou <stylerlight101@gmail.com>
2018-08-21 16:52:48 +0000

    automatically backed up by learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,13 @@
 class Model
-  # Replace with Dog clas
+  # Replace with Dog class
+  attr_accessor :nanme, :breed, :age
+ 
+  DOGS = []
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+    DOGS << self
+    
+  end
 end
